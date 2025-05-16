5be2043aa9dd8c8c41bd0d1b89378dc12a980447
ozerbaykal <ozerbaykal@192.168.1.3>
2025-05-17 02:44:29 +0300

    droptable createtable and insterttable added

--- src/screens/contacts/index.jsx
+++ src/screens/contacts/index.jsx
@@ -1,42 +1,114 @@
-import {View, Text} from 'react-native';
+import {View, Text, FlatList, TouchableOpacity} from 'react-native';
 import defaultScreenStyle from '../../styles/defaultScreenStyle';
-import { useEffect } from 'react';
+import {useEffect, useState} from 'react';
 import SQLite from 'react-native-sqlite-storage';
+import Icon from '@react-native-vector-icons/ionicons';
 
-SQLite.enablePromise(true);
+SQLite.enablePromise(false);
+
+const db = SQLite.openDatabase({
+  name: 'ContactsDataBase',
+  location: 'default',
+});
 
 const Contacts = () => {
-  useEffect(() => {
-    const createContactsTable = async () => {
-      try {
-        const db = await SQLite.openDatabase({name: 'ContactsDataBase', location: 'default'});
-        await db.transaction(async txn => {
-          await txn.executeSql(
-            `CREATE TABLE IF NOT EXISTS users (
-              id INTEGER PRIMARY KEY AUTOINCREMENT,
-              name VARCHAR(100),
-              surname VARCHAR(100),
-              phone INTEGER,
-              email VARCHAR(50),
-              job VARCHAR(500)
-            );`
-          );
+  const [users, setUsers] = useState([]);
+  const dropTable = () => {
+  db.transaction(txn => {
+    txn.executeSql(
+      'DROP TABLE IF EXISTS users',
+      [],
+      () => {
+        console.log('users tablosu silindi');
+      },
+      error => {
+        console.log('Drop Hatası:', error.message);
+      },
+    );
+  });
+};
+  const createContactsTable = () => {
+    db.transaction(txn => {
+      txn.executeSql(
+        'CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(100), surname VARCHAR(100), phone INTEGER, email VARCHAR(50),adress VARCHAR(500), job VARCHAR(30))',
+        [],
+        (sqlTxn, res) => {
           console.log('Tablo oluşturuldu');
-        });
-      } catch (error) {
-        console.log('SQLite Hatası:', error);
-      }
-    };
+        },
+        error => {
+          console.log('Hata:', error.message);
+        },
+      );
+    });
+  };
 
-    console.log('Contacts mount oldu');
+  const getContacts = () => {
+    db.transaction(txn => {
+      txn.executeSql(
+        'SELECT * FROM users',
+        [],
+        (sqlTxn, res) => {
+          if (res.rows.length > 0) {
+            const temp =[]
+            for (let i = 0; i < res.rows.length; i++) {
+              let item = res.rows.item(i);
+              temp.push(item)
+            }
+              setUsers(temp);
+
+          }else{
+            setUsers([])
+          }
+          console.log(' gelen veriler', res.rows);
+        },
+        error => {
+          console.log('Hata:', error.message);
+        },
+      );
+    });
+  };
+
+  const addNewContact = (name, surname, phone, email, adress, job) => {
+    db.transaction(txn => {
+      txn.executeSql(
+        'INSERT INTO users (name,surname,phone,email,adress,job) VALUES(?,?,?,?,?,?)',
+        [name, surname, phone, email, adress, job],
+        (sqlTxn, res) => {
+          console.log('kişi eklendi');
+          getContacts()
+        },
+        error => {
+          console.log('Hata:', error.message);
+        },
+      );
+    });
+  };
+
+  useEffect(() => {
+    dropTable()
     createContactsTable();
+    getContacts();
   }, []);
 
   return (
     <View style={defaultScreenStyle.container}>
-      <Text>Contacts</Text>
+      <FlatList
+        data={users}
+        renderItem={({item}) => <Text>{item.name}</Text>}
+      />
+      <TouchableOpacity
+      onPress={()=>addNewContact("özer","baykal","5084888","baykal@gmail.com","istanbul","yazılım")}
+        style={{
+          position: 'absolute',
+          right: 20,
+          bottom: 20,
+          backgroundColor: 'green',
+          padding: 20,
+          borderRadius: 100,
+        }}>
+        <Icon name="add" size={30} color={'black'} />
+      </TouchableOpacity>
     </View>
   );
 };
-
 export default Contacts;

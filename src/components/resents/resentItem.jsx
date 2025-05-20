b36ba7f58a9595b0a706ff87c23e031e6288fd53
ozerbaykal <ozerbaykal@192.168.1.13>
2025-05-21 01:25:07 +0300

    render resentsCall

--- src/components/resents/resentItem.jsx
+++ src/components/resents/resentItem.jsx
@@ -1,14 +1,103 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {Pressable, StyleSheet, Text, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import SQLite from 'react-native-sqlite-storage';
+import {convertFullName} from '../../utils/functions';
+import Colors from '../../theme/colors';
+import Avatar from '../contacts/avatar';
+import {sizes} from '../../utils/constants';
+import Icon from '@react-native-vector-icons/ionicons'
 
 const ResentItem = ({item}) => {
+  const db = SQLite.openDatabase({
+    name: 'ContactsDataBase',
+    location: 'default',
+  });
+
+  const [user, setUser] = useState({});
+  const getUser = () => {
+    db.transaction(txn => {
+      txn.executeSql(
+        `SELECT * FROM users WHERE id = ${item.resent_id}`,
+        [],
+        (sqlTxn, res) => {
+          if (res.rows.length > 0) {
+            for (let i = 0; i < res.rows.length; i++) {
+              let item = res.rows.item(i);
+              if (user) {
+                setUser(item);
+                console.log(user);
+              } else {
+                setUser({});
+              }
+            }
+          }
+        },
+        error => {
+          console.log('Hata:', error.message);
+        },
+      );
+    });
+  };
+  useEffect(() => {
+    getUser();
+  }, []);
+
   return (
-    <View>
-      <Text>{item.resent_id}</Text>
-    </View>
-  )
-}
+    <Pressable style={styles.container}>
+      <View style={styles.avatarContainer}>
+        <Avatar item={user} size={sizes.SMALL} />
+      </View>
 
-export default ResentItem
+      <View style={styles.infoContainer}>
+        <Text style={styles.name}>
+          {convertFullName(user.name, user.surname)}
+        </Text>
+        <Text style={styles.job}>{item.date}</Text>
+      </View>
 
-const styles = StyleSheet.create({})
+      <View style={styles.callTypeContainer}>
+        {item?.callType == 'incoming' ? (
+          <Icon name="arrow-undo-outline" size={30} color={Colors.RED} />
+        ) : (
+          <Icon name="arrow-redo-outline" size={30} color={Colors.GREEN} />
+        )}
+      </View>
+    </Pressable>
+  );
+};
+
+export default ResentItem;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 5,
+  },
+  name: {
+    fontSize: 18,
+    fontWeight: '700',
+    margin: 5,
+    color: Colors.BLACK,
+  },
+  job: {
+    fontSize: 14,
+
+    fontWeight: '700',
+    color: Colors.GRAY,
+    margin: 5,
+  },
+  infoContainer: {
+    flex: 4,
+    marginHorizontal:10
+  },
+  avatarContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  callTypeContainer:{
+    marginHorizontal:10
+  }
+});
